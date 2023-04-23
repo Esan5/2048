df349d8cf62d4af4f0a928bbc0b5cc356823f209
Ethan Kaji <ethan.kaji@gmail.com>
2023-04-23 04:34:14 -0400

    moved the game.hpp file into the src directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-#include "game.cpp"
+#include "../headers/game.hpp"
 
 int main() {
   std::vector<std::vector<int> > board = {{2, 2, 2, 2}, {1, 2, 3, 4}, {4, 3, 2, 1}, {1, 1, 1, 1}};
