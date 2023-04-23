0f68b8bbfad034d8ba4b4cf76e77046c2236eece
Ethan Kaji <ethan.kaji@gmail.com>
2023-04-23 03:42:51 -0400

    initial commit for source files.

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+#include "game.cpp"
+
+int main() {
+  std::vector<std::vector<int> > board = {{2, 2, 2, 2}, {1, 2, 3, 4}, {4, 3, 2, 1}, {1, 1, 1, 1}};
+
+  game g(board);
+  std::cout << g.to_string() << '\n' << '\n';
+
+  g.flipVertical();
+  std::cout << g.to_string() << '\n' << '\n';
+
+  g.flipHorizontal();
+  std::cout << g.to_string() << '\n' << '\n';
+
+  g.rotate();
+  std::cout << g.to_string() << '\n' << '\n';
+}
