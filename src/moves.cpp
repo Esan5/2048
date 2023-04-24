5bf06cc817348f10824ed8e0cdc9ba8e9822ccf7
Ethan Kaji <ethan.kaji@gmail.com>
2023-04-24 02:24:48 -0400

    completed move left, right, up, down

--- src/moves.cpp
+++ src/moves.cpp
@@ -1,37 +1,40 @@
 #include "moves.hpp"
-#include "game.hpp"
+#include "bitboard.hpp"
 
 /**
  * Makes an up movement on the board.
  */
 uint64_t moves::moveUp(uint64_t board) {
-  return board;
+  return bitboard::rotate(moves::moveLeft(bitboard::counterRotate(board)));
 }
 
 /**
  * Makes a left movement on the board.
  */
 uint64_t moves::moveLeft(uint64_t board) {
-  return (uint64_t) moves::condenseRow((board & game::row_1) >> 48) << 48 |
-         (uint64_t) moves::condenseRow((board & game::row_2) >> 32) << 32 |
-                    moves::condenseRow((board & game::row_3) >> 16) << 16 |
-                    moves::condenseRow((board & game::row_4));
+  return (uint64_t) moves::condenseRow((board & bitboard::row_1) >> 48) << 48 |
+         (uint64_t) moves::condenseRow((board & bitboard::row_2) >> 32) << 32 |
+                    moves::condenseRow((board & bitboard::row_3) >> 16) << 16 |
+                    moves::condenseRow((board & bitboard::row_4));
 }
 
 /**
  * Makes a right movement on the board.
  */
 uint64_t moves::moveRight(uint64_t board) {
-  return board;
+  return bitboard::flipHorizontal(moves::moveLeft(bitboard::flipHorizontal(board)));
 }
 
 /**
  * Makes a down movement on the board.
  */
 uint64_t moves::moveDown(uint64_t board) {
-  return board;
+  return bitboard::counterRotate(moves::moveLeft(bitboard::rotate(board)));
 }
 
+/**
+ * Condenses a row towards the left.
+ */
 uint16_t moves::condenseRow(uint16_t row) {
   if (!row) return row;
 
@@ -52,7 +55,7 @@ uint16_t moves::condenseRow(uint16_t row) {
     result |= (row & 0xF000) + 0x1000;
 
     if ((row & 0x00F0) == ((row & 0x000F) << 4)) {
-      result |= (row & 0x00F0) << 5;
+      result |= ((row & 0x00F0) + 0x0010) << 4;
     } else {
       result |= (row & 0x00FF) << 4;
     }
