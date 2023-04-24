b457b6c8bb2f50cdec5ee8f54a214d2a736f0bc9
Ethan Kaji <ethan.kaji@gmail.com>
2023-04-24 19:50:01 -0400

    updated to use precomputed tables for move generation.

--- src/moves.hpp
+++ src/moves.hpp
@@ -2,14 +2,22 @@
 #define MOVES_HPP
 
 #include <cstdint>
+#include <array>
 
 namespace moves {
-  uint16_t condenseRow(uint16_t row);
+  static uint16_t moveTableLeft[0xFFFF];
+  static uint16_t moveTableRight[0xFFFF];
+
+  constexpr uint16_t condenseRow(uint16_t row);
 
   uint64_t moveDown(uint64_t board);
   uint64_t moveUp(uint64_t board);
   uint64_t moveRight(uint64_t board);
   uint64_t moveLeft(uint64_t board);
+
+  void generateMovesLeft();
+  void generateMovesRight();
+  void initializeMoveTables();
 };  // namespace moves
 
 #endif
