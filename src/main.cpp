a717d97a6e95c5c65554274b7a5ef10d13c2c379
Ethan Kaji <ethan.kaji@gmail.com>
2023-04-24 02:28:35 -0400

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@ int main() {
   std::vector<std::vector<int> > test = {{2, 2, 2, 2}, {1, 2, 3, 4}, {4, 3, 2, 1}, {1, 1, 1, 1}};
 
   uint64_t board = game::getBoard(test);
+  std::cout << game::to_string(board) << '\n' << '\n';
 
-  board = moves::moveLeft(board);
+  board = moves::moveRight(board);
   std::cout << game::to_string(board) << '\n' << '\n';
 }
