382a9e2fb8a5ac01eeb4815d715f3c92bc11e414
Ethan Kaji <ethan.kaji@gmail.com>
2023-04-25 03:51:22 -0400

    Compile time lookup tables with constexpr

--- src/moves.cpp
+++ src/moves.cpp
@@ -2,13 +2,6 @@
 #include "bitboard.hpp"
 
 /**
- * Makes an up movement on the board.
- */
-uint64_t moves::moveUp(uint64_t board) {
-  return bitboard::counterRotate(moves::moveRight(bitboard::rotate(board)));
-}
-
-/**
  * Makes a left movement on the board.
  */
 uint64_t moves::moveLeft(uint64_t board) {
@@ -29,81 +22,15 @@ uint64_t moves::moveRight(uint64_t board) {
 }
 
 /**
+ * Makes an up movement on the board.
+ */
+uint64_t moves::moveUp(uint64_t board) {
+  return bitboard::counterRotate(moves::moveRight(bitboard::rotate(board)));
+}
+
+/**
  * Makes a down movement on the board.
  */
 uint64_t moves::moveDown(uint64_t board) {
   return bitboard::counterRotate(moves::moveLeft(bitboard::rotate(board)));
-}
-
-/**
- * Condenses a row towards the left.
- */
-constexpr uint16_t moves::condenseRow(uint16_t row) {
-  if (!row) return row;
-
-  uint16_t result = 0;
-
-  while (!(row & 0xF000)) {
-    row = row << 4;
-  }
-
-  while (!(row & 0x0F00) && (row & 0x00FF)) {
-    row = (row & 0xF000) | ((row & 0x00FF) << 4);
-  }
-
-  if (!(row & 0x00F0))
-    row = (row & 0xFF00) | ((row & 0x000F) << 4);
-
-  if ((row < 0xF000) && (row & 0xF000) == ((row & 0x0F00) << 4)) {
-    result |= (row & 0xF000) + 0x1000;
-
-    if ((row & 0x00F0) != 0x00F0 && (row & 0x00F0) == ((row & 0x000F) << 4)) {
-      result |= ((row & 0x00F0) + 0x0010) << 4;
-    } else {
-      result |= (row & 0x00FF) << 4;
-    }
-
-  } else if ((row & 0x0F00) != 0x0F00 && (row & 0x0F00) == ((row & 0x00F0) << 4)) {
-    result |= (row & 0xF000) | ((row & 0x0F00) + 0x0100) | ((row & 0x000F) << 4);
-  } else if ((row & 0x00F0) != 0x00F0 && (row & 0x00F0) == ((row & 0x000F) << 4)) {
-    result |= (row & 0xFF00) | ((row & 0x00F0) + 0x0010);
-  } else {
-    result = row;
-  }
-
-  return result;
-}
-
-/**
- * Initialize a move table for the left movement
- */
-void moves::generateMovesLeft() {
-  for (uint16_t i = 0; i < 0xFFFF; i++)
-    moves::moveTableLeft[i] = moves::condenseRow(i);
-}
-
-/**
- * Initialize the move table for right movement
- */
-void moves::generateMovesRight() {
-  for (uint16_t i = 0; i < 0xFFFF; i++) {
-    uint16_t j = ((i & 0xF000) >> 12) |
-                 ((i & 0x0F00) >> 4)  |
-                 ((i & 0x00F0) << 4)  |
-                 ((i & 0x000F) << 12);
-    uint16_t res_j = moves::moveTableLeft[j];
-    moves::moveTableRight[i] = ((res_j & 0xF000) >> 12) |
-                               ((res_j & 0x0F00) >> 4)  |
-                               ((res_j & 0x00F0) << 4)  |
-                               ((res_j & 0x000F) << 12);
-  }
-
-}
-
-/**
- * Initialize the move tables for left and right
- */
-void moves::initializeMoveTables() {
-  moves::generateMovesLeft();
-  moves::generateMovesRight();
 }
--- src/moves.hpp
+++ src/moves.hpp
@@ -5,19 +5,72 @@
 #include <array>
 
 namespace moves {
-  static uint16_t moveTableLeft[0xFFFF];
-  static uint16_t moveTableRight[0xFFFF];
-
-  constexpr uint16_t condenseRow(uint16_t row);
-
   uint64_t moveDown(uint64_t board);
   uint64_t moveUp(uint64_t board);
   uint64_t moveRight(uint64_t board);
   uint64_t moveLeft(uint64_t board);
 
-  void generateMovesLeft();
-  void generateMovesRight();
-  void initializeMoveTables();
+  /**
+  * Condenses a row towards the left.
+  */
+  constexpr uint16_t condenseRow(uint16_t row) {
+    // If the row is empty, change nothing
+    if (!row) return row;
+
+    uint16_t result = row;
+
+    // Condense the row so that all tiles are as far left as possible
+    if (!(result & 0x00F0))
+      result = (result & 0xFF00) | ((result & 0x000F) << 4);
+    if (!(result & 0x0F00))
+      result = (result & 0xF000) | ((result & 0x00FF) << 4);
+    if (!(result & 0xF000))
+      result = result << 4;
+
+    // Check if the first two tiles match, if not, check the middle tiles
+    if ((result & 0xF000) == ((result & 0x0F00) << 4)) {
+      result = ((result & 0xF000) | (result & 0x00FF)) + 0x1000;
+    } else if ((result & 0x0F00) && (result & 0x0F00) == ((result & 0x00F0) << 4)) {
+      // If they match, early return the merged output
+      return ((result & 0xFF00) | ((result & 0x000F) << 4)) + 0x0100;
+    }
+
+    // Check if the last two tiles match
+    if ((result & 0x00F0) && (result & 0x00F0) == ((result & 0x000F) << 4)) {
+      result = (result & 0xFFF0) + 0x0010;
+    }
+
+    // Make sure there isn't any remaining spaces
+    if (!(result & 0x0F00))
+      result = (result & 0xF000) | ((result & 0x00FF) << 4);
+
+    return result;
+  }
+
+  // These tables are made at compile time to store the results of moves
+  constexpr std::array<uint16_t, 0xFFFF> moveTableLeft = [] {
+    std::array<uint16_t, 0xFFFF> result {};
+    for (uint16_t i = 0; i < 0xFFFF; i++)
+      result[i] = condenseRow(i);
+    return result;
+  } ();
+
+  constexpr std::array<uint16_t, 0xFFFF> moveTableRight = [] {
+    std::array<uint16_t, 0xFFFF> result {};
+    for (uint16_t i = 0; i < 0xFFFF; i++) {
+      uint16_t j = ((i & 0xF000) >> 12) |
+                  ((i & 0x0F00) >> 4)  |
+                  ((i & 0x00F0) << 4)  |
+                  ((i & 0x000F) << 12);
+      uint16_t res_j = moveTableLeft[j];
+      result[i] = ((res_j & 0xF000) >> 12) |
+                  ((res_j & 0x0F00) >> 4)  |
+                  ((res_j & 0x00F0) << 4)  |
+                  ((res_j & 0x000F) << 12);
+    }
+    return result;
+  } ();
+
 };  // namespace moves
 
 #endif
