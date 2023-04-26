e58bfde72e748c6a8ddfcdc2b934b254649d1170
Ethan Kaji <ethan.kaji@gmail.com>
2023-04-26 04:42:45 -0400

    constexpr countTiles

--- src/game.hpp
+++ src/game.hpp
@@ -12,7 +12,9 @@ namespace game {
 
   uint64_t openSpaces(uint64_t board);
 
-  uint8_t countTiles(uint64_t board);
+  constexpr uint8_t countTiles(uint64_t board);
+
+  uint8_t maxTile(uint64_t board);
 
   uint64_t populateBoard(uint64_t board);
 
