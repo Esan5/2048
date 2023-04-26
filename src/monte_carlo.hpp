12f8264be457f2eff1eba7f86579a5ff4708102c
Ethan Kaji <ethan.kaji@gmail.com>
2023-04-26 04:43:22 -0400

    slight adjustments to the number of trials

--- src/monte_carlo.hpp
+++ src/monte_carlo.hpp
@@ -2,12 +2,12 @@
 #include "moves.hpp"
 
 class MonteCarlo {
-  static constexpr uint16_t NUM_TRIALS = 300;
+  static constexpr uint16_t NUM_TRIALS = 500;
 
  public:
   static moves::type bestMove(uint64_t board);
 
  private:
   static uint64_t randomMove(uint64_t board);
-  static uint64_t scoreState(uint64_t board);
+  static uint32_t scoreState(uint64_t board);
 };
