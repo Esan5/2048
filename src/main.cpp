949bb13dd8032bb832afc83331591d6d13ae0c47
Ethan Kaji <ethan.kaji@gmail.com>
2023-05-14 19:06:17 -0400

    Implementation of a genetic style algorithm in hopes of converging to optimal hyperparameters.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,112 @@
 #include <unordered_map>
 #include <random>
 #include <future>
+#include <array>
+#include <queue>
 
 #include "hyperparameter.hpp"
 #include "expectiminimax.hpp"
 #include "game.hpp"
+
+parameter::individual cross(const parameter::individual &i1, const parameter::individual &i2) {
+  parameter::individual result;
+  result.edge = (i1.edge + i2.edge) / 2;
+  result.zero = (i1.zero + i2.zero) / 2;
+  result.mono = (i1.mono + i2.mono) / 2;
+  result.merge = (i1.merge + i2.merge) / 2;
+
+  return result;
+}
+
+void mutate(parameter::individual &i) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, 2);
+
+  std::array<float, 3> mutation {1, 0.80, 1.25};
+
+  i.zero = i.zero * mutation[dis(gen)];
+  i.merge = i.merge * mutation[dis(gen)];
+  i.edge = i.edge * mutation[dis(gen)];
+  i.mono = i.mono * mutation[dis(gen)];
+
+}
+
+void print_individual(parameter::individual const &i) {
+  std::cout << "Edge: " << i.edge << "\n";
+  std::cout << "Zero: " << i.zero << "\n";
+  std::cout << "Mono: " << i.mono << "\n";
+  std::cout << "Merge: " << i.merge << "\n\n";
+}
+
+void genetic() {
+  float best_mono;
+  float best_zero;
+  float best_edge;
+  float best_merge;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(0, 1000);
+
+  std::array<parameter::individual, 16> population {};
+
+  for (auto i = 0; i < 16; i++) {
+    population[i].mono = dis(gen);
+    population[i].zero = dis(gen);
+    population[i].edge = dis(gen);
+    population[i].merge = dis(gen);
+
+  }
+
+  auto cmp = [] (std::pair<float, parameter::individual> p1, std::pair<float, parameter::individual> p2) {
+    return std::get<float>(p1) < std::get<float>(p2);
+  };
+
+  for (;;) {
+    std::array<std::future<float>, 16> results;
+
+    for (auto i = 0; i < 16; i++) {
+      results[i] = std::async(parameter::evaluateIndividual, population[i]);
+    }
+
+    std::priority_queue<std::pair<float, parameter::individual>,
+                        std::vector<std::pair<float, parameter::individual> >,
+                        decltype(cmp)> pq(cmp);
+
+    for (auto i = 0; i < 16; i++) {
+      pq.push(std::make_pair(results[i].get(), population[i]));
+    }
+
+    std::array<parameter::individual, 4> top_4 {};
+    population[0] = std::get<parameter::individual>(pq.top());
+    std::cout << "Top Score: " << std::get<float>(pq.top()) << "\n";
+    print_individual(population[0]);
+    pq.pop();
+    population[1] = std::get<parameter::individual>(pq.top());
+    std::cout << "Second: " << std::get<float>(pq.top()) << "\n";
+    print_individual(population[1]);
+    pq.pop();
+    population[2] = std::get<parameter::individual>(pq.top());
+    std::cout << "Third: " << std::get<float>(pq.top()) << "\n";
+    print_individual(population[2]);
+    pq.pop();
+    population[3] = std::get<parameter::individual>(pq.top());
+    std::cout << "Fourth: " << std::get<float>(pq.top()) << "\n";
+    print_individual(population[3]);
+    pq.pop();
+
+    int count = 4;
+    for (auto i = 0; i < 4; i++) {
+      for (auto j = i + 1; j < 4; j++) {
+        population[count++] = cross(population[i], population[j]);
+      }
+    }
+
+    for (auto i = 4; i < 16; i++)
+      mutate(population[i]);
+  }
+}
 
 void train_hypers() {
   const int num_trials = 8;
@@ -37,7 +139,8 @@ void train_hypers() {
     std::array<std::future<uint32_t>, num_trials> results;
 
     for (int i = 0; i < results.size(); i++) {
-      results[i] = std::async(parameter::evaluateHueristics, mono, zero, edge, merge);
+      parameter::individual in = {.mono = mono, .zero = zero, .edge = edge, .merge = merge};
+      results[i] = std::async(std::launch::async, parameter::evaluateHueristics, in);
     }
 
     for (int i = 0; i < results.size(); i++) {
@@ -66,13 +169,5 @@ void train_hypers() {
 }
 
 int main() {
-
-  uint64_t board = game::populateBoard(game::populateBoard(0));
-
-  while (!game::gameOver(board)) {
-    board = moves::move(board, exp::bestMove(board));
-    board = game::populateBoard(board);
-    std::cout << game::to_string(board) << "\n\n";
-  }
-
+  genetic();
 }
